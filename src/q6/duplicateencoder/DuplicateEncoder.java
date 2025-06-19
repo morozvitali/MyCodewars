f430003141fd9da6e05a782126bffb943facf401
morozvitali <morozvitali99@gmail.com>
2025-06-19 16:48:28 +0300

    Dublicate encoders q6 -> realisation

---
+++ src/q6/duplicateencoder/DuplicateEncoder.java
@@ -0,0 +1,17 @@
+package q6.duplicateencoder;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DuplicateEncoder {
+    static String encode(String word){
+        Map<Character, Long> mapping = word.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        return word.chars()
+                .mapToObj(c -> (char) c)
+                .map(ch -> mapping.get(ch) > 1 ? ")" : "(")
+                .collect(Collectors.joining());
+    }
+}
