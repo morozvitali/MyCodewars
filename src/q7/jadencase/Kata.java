f918f04b520d8e98cf55f415eabb07461b87e95b
morozvitali <morozvitali99@gmail.com>
2025-08-08 16:33:19 +0300

    opposite q8 task

---
+++ src/q7/jadencase/Kata.java
@@ -0,0 +1,16 @@
+package q7.jadencase;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Kata {
+    public String toJadenCase(String phrase) {
+
+        return  phrase == null ? null :
+                phrase.isEmpty() ? null :
+                        Arrays.stream(phrase.split(" "))
+                                .map(w->Character.toUpperCase(w.charAt(0)) + w.substring(1))
+                                .collect(Collectors.joining(" "));
+    }
+}
